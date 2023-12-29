45f2a9ee0bc935349336de1ad3e98e8f316c1f18
MoYasser1 <mohamed0yasser@gmail.com>
2023-12-30 01:41:20 +0200

    deep

--- script.js
+++ script.js
@@ -9,7 +9,6 @@ function login() {
             sessionStorage.setItem("username", username);
             window.location.href = "home.html";
         } else {
-            
             alert("Invalid username or password");
         }
     } else {
@@ -18,91 +17,118 @@ function login() {
 }
 
 
+
+
 function signup() {
-    // Get form inputs
     var fullName = document.getElementById("fullname").value;
     var email = document.getElementById("email").value;
     var password = document.getElementById("password").value;
     var confirmPassword = document.getElementById("confirmPassword").value;
 
-    // Validate full name
     if (fullName.trim() === "") {
         alert("Please enter your full name");
         return;
     }
 
-    // Validate email
     var emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
     if (!emailRegex.test(email)) {
         alert("Please enter a valid email address");
         return;
     }
 
-    // Validate password
     if (password.length < 6) {
         alert("Password must be at least 6 characters long");
         return;
     }
 
-    // Validate password match
     if (password !== confirmPassword) {
         alert("Passwords do not match");
         return;
     }
 
+    if (localStorage.getItem(email)) {
+        alert("User already exists. Please login instead.");
+        return;
+    }
+    var user = {
+        fullName: fullName,
+        password: password
+    };
+    localStorage.setItem(email, JSON.stringify(user));
 
     alert("Signup successful!");
 }
 
 
-// script.js
-
 document.addEventListener("DOMContentLoaded", function () {
-    // Replace 'YOUR_API_KEY' with your actual API key
-    const apiKey = '81570769';
-    const apiUrl = `https://www.omdbapi.com/?s=batman&apikey=${apiKey}`;
+    apiKey = '81570769';
+    defaultSearchTerm = 'deep';
+    apiUrl = `https://www.omdbapi.com/?s=${defaultSearchTerm}&apikey=${apiKey}`;
 
-    // Get the container element to display movies
-    const container = document.querySelector('.container');
+    container = document.querySelector('.container');
+    searchInput = document.getElementById('searchInput');
+    searchForm = document.getElementById('searchForm');
+    username = sessionStorage.getItem('username');
 
-    // Fetch data from the API
-    fetch(apiUrl)
-        .then(response => response.json())
-        .then(data => {
-            // Display the list of movies
-            displayMovies(data.Search);
-        })
-        .catch(error => console.error('Error fetching data:', error));
+    if (username) {
+        usernameDisplay = document.getElementById('usernameDisplay');
+        usernameDisplay.textContent = `Welcome, ${username}!`;
+        usernameDisplay.style.color = 'white'; 
+    }
 
-    // Function to display movies in the container
+    fetchMovies(apiUrl);
+
+    document.getElementById('searchButton').addEventListener('click', searchMovies);
+
+    searchForm.addEventListener('submit', function (event) {
+        event.preventDefault();
+        searchMovies();
+    });
+
+    let typingTimer;
+    doneTypingInterval = 500;
+
+    searchInput.addEventListener('input', function () {
+        clearTimeout(typingTimer);
+        typingTimer = setTimeout(searchMovies, doneTypingInterval);
+    });
+
+    function searchMovies() {
+        searchTerm = searchInput.value;
+        searchUrl = `https://www.omdbapi.com/?s=${searchTerm}&apikey=${apiKey}`;
+        fetchMovies(searchUrl);
+    }
+
+    function fetchMovies(url) {
+        fetch(url)
+            .then(response => response.json())
+            .then(data => {
+                displayMovies(data.Search);
+            })
+            .catch(error => console.error('Error fetching data:', error));
+    }
+
     function displayMovies(movies) {
         const moviesList = document.getElementById('moviesList');
-
-        // Check if movies exist
+        moviesList.innerHTML = '';
         if (movies && movies.length > 0) {
-            // Loop through the movies and create list items with images
             movies.forEach(movie => {
-                const listItem = document.createElement('li');
+                listItem = document.createElement('li');
 
-                // Create an image element
-                const movieImage = document.createElement('img');
+                movieImage = document.createElement('img');
                 movieImage.src = movie.Poster;
                 movieImage.alt = movie.Title;
 
-                // Create a paragraph element for the movie title
-                const titleParagraph = document.createElement('p');
+                titleParagraph = document.createElement('p');
                 titleParagraph.textContent = movie.Title;
 
-                // Append the image and title to the list item
                 listItem.appendChild(movieImage);
                 listItem.appendChild(titleParagraph);
 
-                // Append the list item to the movies list
                 moviesList.appendChild(listItem);
             });
         } else {
-            // Display a message if no movies are found
-            const message = document.createElement('p');
+            message = document.createElement('p');
             message.textContent = 'No movies found.';
             container.appendChild(message);
         }
